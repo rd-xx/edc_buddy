d91ff136f5699959375e0577f101044e49bee75d
rdx <reydux95@gmail.com>
2023-01-22 18:31:40 +0100

    feat(HomeScreen): show answers in home screen, if any

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,61 +1,71 @@
+import 'package:edc_buddy/api/get_questions.dart';
 import 'package:edc_buddy/screens/questions.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  /// Generate a list of Row from the stored answers.
+  List<Row> _getAnswers() {
+    List<Row> rows = [];
+
+    for (var question in apiQuestions) {
+      if (!GetStorage().hasData(question["Key"]!)) continue;
+      String answer = GetStorage().read(question["Key"]!);
+
+      rows.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "${question["Label"]} : ",
+            style: const TextStyle(fontSize: 24),
+          ),
+          Text(
+            answer,
+            style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent),
+          ),
+        ],
+      ));
+    }
+
+    return rows;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text("Étude de cas - Buddy"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-                child: const Text("Aller aux questions."),
-                onPressed: () => Get.to(const QuestionsScreen())),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade300, elevation: 4),
+                child: const Text(
+                  "Aller aux questions",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                onPressed: () => Get.to(() => const QuestionsScreen())),
+            const Divider(
+              height: 50,
+              indent: 30,
+              endIndent: 30,
+              thickness: 1,
+            ),
+            ..._getAnswers()
           ],
         ),
       ),
