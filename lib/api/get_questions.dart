95f1660ef800da3eed280ca5203983aa36bc611c
rdx <reydux95@gmail.com>
2023-01-22 17:02:36 +0100

    feat(QuestionScreen): new layout prototype

--- lib/api/get_questions.dart
+++ lib/api/get_questions.dart
@@ -1,12 +1,14 @@
-var questions = [
+var apiQuestions = [
   {
     "Label": "Comment tu _t'appeles ?",
     "Description": "Les pseudos ne sont pas acceptés.",
-    "InputType": "str"
+    "InputType": "str",
+    "InputHint": "Ton prénom"
   },
   {
     "Label": "Quel _âge as-tu ?",
-    "Description": "Moins de 30 seulement.",
-    "InputType": "nbr"
+    "Description": "Moins de 30 ans seulement.",
+    "InputType": "nbr",
+    "InputHint": "Ton âge"
   }
 ];
